c89bae37ef1e88d5213983072db2df900dc65524
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2020-09-30 19:51:13 -0300

    Initial Version

---
+++ src/test/java/com/ferfox1981/springbatchrest/SpringBatchRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferfox1981.springbatchrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
